dfb511a80ef12ae10054cd39199b32dafc0a73ac
Doray Hong <hongduhui@qq.com>
2016-08-13 21:40:40 +0800

    feat(example): add a simple example - Hello

---
+++ example/hello/index.js
@@ -0,0 +1,33 @@
+import angular from 'angular'
+import Vue from 'vue'
+import '../../build/ngVue.es'
+
+angular.module('vue.components', ['ngVue'])
+  .controller('MainController', function () {
+    this.person = {
+      firstName: 'The',
+      lastName: 'World',
+      description: 'ngVue helps you use Vue components in your angular application ' +
+                   'so that you are able to create a faster and reactive web interfaces.'
+    }
+  })
+  .value('HelloComponent', Vue.component('hello-component', {
+    props: {
+      firstName: String,
+      lastName: String,
+      description: String
+    },
+    render (h) {
+      return (
+        <div class="card blue-grey darken-1">
+          <div class="card-content white-text">
+            <span class="card-title">Hi, {this.firstName} {this.lastName}</span>
+            <p>{this.description}</p>
+          </div>
+          <div class="card-action">
+            <a href="https://vuejs.org/guide/overview.html">Vue.js</a>
+          </div>
+        </div>
+      )
+    }
+  }))
---
+++ example/webpack.config.js
@@ -0,0 +1,71 @@
+var path = require('path')
+var fs = require('fs')
+var webpack = require('webpack')
+
+function getExampleEntries () {
+  var dir = 'example'
+  var entry = {}
+
+  fs.readdirSync('./' + dir).filter(function (name) {
+    return fs.statSync('./' + dir + '/' + name).isDirectory()
+  }).forEach(function (name) {
+    entry[name] = path.resolve(__dirname, './' + name)
+  })
+
+  return entry
+}
+
+module.exports = {
+  entry: getExampleEntries(),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/assets/',
+    filename: '[name].build.js'
+  },
+  resolveLoader: {
+    root: path.join(__dirname, '../node_modules')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file',
+        query: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    port: 12345
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ])
+}
