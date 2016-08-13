8a1ec9a73f9d88f110b14e45a3e5c19fa0308eec
Doray Hong <hongduhui@qq.com>
2016-08-13 21:37:53 +0800

    chore(project): initialize the project

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import babel from 'rollup-plugin-babel'
+
+export default {
+  entry: 'src/index.js',
+  moduleName: 'ngVue',
+  moduleId: 'ngVue',
+  plugins: [
+    babel()
+  ],
+  globals: {
+    vue: 'Vue',
+    angular: 'angular'
+  },
+  targets: [
+    { dest: 'build/ngVue.es.js', format: 'es' },
+    { dest: 'build/ngVue.umd.js', format: 'umd' }
+  ]
+}
