314c7f952868d70b8de08d87fc32f917cf88e214
Doray Hong <hongduhui@qq.com>
2017-01-08 19:35:19 +0800

    chore(rollup): fix a bundle issue on vue jsx helper

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,20 @@
 import babel from 'rollup-plugin-babel'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 
 export default {
   entry: 'src/index.js',
   moduleName: 'ngVue',
   moduleId: 'ngVue',
   plugins: [
+    nodeResolve({
+      browser: true
+    }),
+    commonjs({
+      namedExports: {
+        'node_modules/babel-helper-vue-jsx-merge-props/index.js': ['_mergeJSXProps']
+      } 
+    }),
     babel()
   ],
   globals: {
