6722233a680c3bd79aeb9746df6a6afcd8413c33
Doray Hong <hongduhui@qq.com>
2017-01-17 00:45:16 +0800

    docs(example): add example code for filters plugin

---
+++ example/plugins-filters/tags.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="tags">
+    <div v-for="tag in tags" class="chip">
+      {{ tag | uppercase }}
+    </div>
+  </div>
+</template>
+<style scoped>
+  .tags {
+    text-align: right;
+    margin-bottom: 30px;
+  }
+</style>
+<script>
+  export default {
+    data () {
+      return {
+        tags: ['angular', 'ngVue', 'VueJS']
+      }
+    }
+  }
+</script>
