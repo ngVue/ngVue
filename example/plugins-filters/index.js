d95563690274f2c13363386a2cc13bded6efd3a9
Nicolas Payot <nicolas.payot@gmail.com>
2017-01-17 14:54:47 +0100

    test(vue-component): add unit tests

---
+++ example/plugins-filters/index.js
@@ -0,0 +1,42 @@
+import angular from 'angular'
+import Vue from 'vue'
+import '../../build/ngVue.es'
+import Tags from './tags.vue'
+
+angular.module('vue.components', ['ngVue'])
+  .config(function ($ngVueProvider) {
+    $ngVueProvider.filters.register(['uppercase'])
+  })
+  .filter('uppercase', function () {
+    return (string) => string.toUpperCase()
+  })
+  .controller('MainController', function () {
+    this.person = {
+      firstName: 'The',
+      lastName: 'World',
+      description: 'ngVue helps you use Vue components in your angular application ' +
+                   'so that you are able to create a faster and reactive web interfaces.'
+    }
+  })
+  .value('TagsComponent', Tags)
+  .value('HelloComponent', Vue.component('hello-component', {
+    props: {
+      firstName: String,
+      lastName: String,
+      description: String
+    },
+    render (h) {
+      const uppercase = Vue.filter('uppercase')
+      return (
+        <div class="card blue-grey darken-1">
+          <div class="card-content white-text">
+            <span class="card-title">Hi, {this.firstName} {this.lastName}</span>
+            <p>{uppercase(this.description)}</p>
+          </div>
+          <div class="card-action">
+            <a href="https://vuejs.org/guide/overview.html">Vue.js</a>
+          </div>
+        </div>
+      )
+    }
+  }))
--- src/angular/ngVueLinker.js
+++ src/angular/ngVueLinker.js
@@ -6,22 +6,32 @@ import evalPropValues from '../components/props/evaluateValues'
 import evaluateDirectives from '../directives/evaluateDirectives'
 
 export function ngVueLinker (componentName, jqElement, elAttributes, scope, $injector) {
+  const $ngVue = $injector.has('$ngVue') ? $injector.get('$ngVue') : null
+
   const dataExprsMap = getPropExprs(elAttributes)
   const Component = getVueComponent(componentName, $injector)
   const directives = evaluateDirectives(elAttributes, scope) || []
-  const reactiveData = evalPropValues(dataExprsMap, scope) || {}
-  const reactiveSetter = Vue.set.bind(Vue, reactiveData)
-  const vueInstance = new Vue({
+  const reactiveData = { _v: evalPropValues(dataExprsMap, scope) || {} }
+
+  let vueOptions = {
     el: jqElement[0],
     data: reactiveData,
     render (h) {
-      return <Component {...{ directives }} {...{ props: reactiveData }} />
+      return <Component {...{ directives }} {...{ props: reactiveData._v }} />
     }
-  })
+  }
 
-  watchPropExprs(dataExprsMap, reactiveSetter, elAttributes, scope)
+  if ($ngVue) {
+    const hooks = $ngVue.getVueHooks()
+    vueOptions = { ...vueOptions, ...hooks }
+  }
+
+  const watchDepth = elAttributes.watchDepth
+  watchPropExprs(dataExprsMap, reactiveData, watchDepth, scope)
+
+  const vueInstance = new Vue(vueOptions)
 
   scope.$on('$destroy', () => {
-    vueInstance.$destroy(true)
+    vueInstance.$destroy()
   })
 }
