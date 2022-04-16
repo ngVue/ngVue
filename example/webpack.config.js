3f4ac8176812b0a4855ed0f507103611f3778b2e
Jared McAteer <jared.mcateer@gmail.com>
2022-04-15 21:27:09 -0600

    chore(deps): Update babel, rollup, webpack and jest

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -77,10 +77,5 @@ if (process.env.NODE_ENV === 'production') {
         NODE_ENV: '"production"',
       },
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-      },
-    }),
   ])
 }
--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-BABEL_ENV=build ENTRY=index OUTPUT=index `npm bin`/rollup -c
-BABEL_ENV=build ENTRY=plugins/index OUTPUT=plugins `npm bin`/rollup -c
-BABEL_ENV=build ENTRY=patches/ngfilters OUTPUT=patch-ngfilters `npm bin`/rollup -c
-
-BABEL_ENV=build MIN=true ENTRY=index OUTPUT=index.min `npm bin`/rollup -c
-BABEL_ENV=build MIN=true ENTRY=plugins/index OUTPUT=plugins.min `npm bin`/rollup -c
-BABEL_ENV=build MIN=true ENTRY=patches/ngfilters OUTPUT=patch-ngfilters.min `npm bin`/rollup -c
+BABEL_ENV=build ENTRY=index OUTPUT=index node_modules/.bin/rollup -c
+BABEL_ENV=build ENTRY=plugins/index OUTPUT=plugins node_modules/.bin/rollup -c
+BABEL_ENV=build ENTRY=patches/ngfilters OUTPUT=patch-ngfilters node_modules/.bin/rollup -c
